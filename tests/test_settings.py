d9908d30a9f27e788edc36a3ccf8d355b9660412
Thomas Cellerier <thomas.cellerier@appgate.com>
2022-09-26 09:46:48 +0200

    No need for lazy imports anymore

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,11 +1,13 @@
 import os
 
+from cratere.settings import Settings
+
 
 def test_alternate_hosts():
     os.environ["CRATERE_HOST"] = "foo.example.com"
     os.environ["CRATERE_ALTERNATE_HOSTS"] = '["foobar", "bar", "baz"]'
-    from cratere.settings import Settings
 
     settings = Settings()
+
     assert settings.host == "foo.example.com"
     assert settings.alternate_hosts == ["foobar", "bar", "baz"]
